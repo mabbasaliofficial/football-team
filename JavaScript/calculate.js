beb18edfcf929caa41c1f6631afc8332ab21ae59
mabbasaliofficial <mabbasaliofficial@gmail.com>
2022-08-22 19:38:43 +0600

    Calculator Part Added

--- JavaScript/calculate.js
+++ JavaScript/calculate.js
@@ -1,1 +1,31 @@
-const addedPlayer = document.querySelector('#player-list').childNodes.length
+document.getElementById("player-cost").addEventListener("click", function () {
+  const addedPlayer = document.querySelector("#player-list").childNodes.length;
+
+  const playerFee = document.getElementById("player-fee");
+  const tolalFeeString = playerFee.value;
+  const totalFeeNumber = parseInt(tolalFeeString);
+  const totalFee = totalFeeNumber * addedPlayer;
+
+  const playerExpensesElement = document.getElementById("player-expenses");
+  // const playerExpensesText = playerExpensesElement.innerText;
+  // playerExpensesElement.innerText = totalFee;
+  playerExpensesElement.innerText = totalFee;
+});
+
+document.getElementById("btn-total").addEventListener("click", function () {
+  const playerExpensesElement = document.getElementById("player-expenses");
+  const playerFeeString = playerExpensesElement.innerText;
+  const playerFee = parseInt(playerFeeString);
+
+  const managerFeeElement = document.getElementById("manager-fee");
+  const managerFeeString = managerFeeElement.value;
+  const managerFee = parseInt(managerFeeString);
+
+  const coachFeeElement = document.getElementById("coach-fee");
+  const coachFeeString = coachFeeElement.value;
+  const coachFee = parseInt(coachFeeString);
+
+  const totalFeeElement = document.getElementById("total");
+  const total = playerFee + managerFee + coachFee;
+  totalFeeElement.innerText = total;
+});
--- JavaScript/playerList.js
+++ JavaScript/playerList.js
@@ -111,21 +111,34 @@ document.getElementById("select-sanches").addEventListener("click", function () 
  */
 
 // const playerNumber = parseInt(addedPlayer);
-document.getElementById("player-cost").addEventListener("click", function () {
-  const addedPlayer = document.querySelector("#player-list").childNodes.length;
+// document.getElementById("player-cost").addEventListener("click", function () {
+//   const addedPlayer = document.querySelector("#player-list").childNodes.length;
 
-  const playerFee = document.getElementById("player-fee");
-  const tolalFeeString = playerFee.value;
-  const totalFeeNumber = parseInt(tolalFeeString);
-  const totalFee = totalFeeNumber * addedPlayer;
+//   const playerFee = document.getElementById("player-fee");
+//   const tolalFeeString = playerFee.value;
+//   const totalFeeNumber = parseInt(tolalFeeString);
+//   const totalFee = totalFeeNumber * addedPlayer;
 
-  const playerExpensesElement = document.getElementById("player-expenses");
-  // const playerExpensesText = playerExpensesElement.innerText;
-  // playerExpensesElement.innerText = totalFee;
-  playerExpensesElement.innerText = totalFee;
-});
+//   const playerExpensesElement = document.getElementById("player-expenses");
+//   // const playerExpensesText = playerExpensesElement.innerText;
+//   // playerExpensesElement.innerText = totalFee;
+//   playerExpensesElement.innerText = totalFee;
+// });
 
+// document.getElementById('btn-total').addEventListener('click', function(){
+//     const playerExpensesElement = document.getElementById("player-expenses");
+//     const playerFeeString = playerExpensesElement.innerText;
+//     const playerFee = parseInt(playerFeeString);
 
-document.getElementById('btn-total').addEventListener('click', function(){
-    
-})
+//     const managerFeeElement = document.getElementById("manager-fee");
+//     const managerFeeString = managerFeeElement.value;
+//     const managerFee = parseInt(managerFeeString);
+
+//     const coachFeeElement = document.getElementById('coach-fee');
+//     const coachFeeString = coachFeeElement.value;
+//     const coachFee = parseInt(coachFeeString);
+
+//     const totalFeeElement = document.getElementById("total");
+//     const total = playerFee + managerFee + coachFee;
+//     totalFeeElement.innerText = total;
+// })

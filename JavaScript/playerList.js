f792387c38297fb6fd3d80203d83806733a48c8b
mabbasaliofficial <mabbasaliofficial@gmail.com>
2022-08-23 08:48:09 +0600

    Common Function Added

--- JavaScript/playerList.js
+++ JavaScript/playerList.js
@@ -1,136 +1,18 @@
 document.getElementById("select-messi").addEventListener("click", function () {
-  const playerId = document.getElementById("messi");
-
-  const button = document.getElementById("select-messi");
-
-  const li = document.createElement("li");
-  li.innerText = playerId.innerText;
-  const ol = document.getElementById("player-list");
-
-  const playerList = document.querySelector("#player-list").childNodes.length;
-  if (playerList < 5) {
-    ol.appendChild(li);
-    button.disabled = true;
-    button.style.backgroundColor = "gray";
-  } else {
-    alert("There is no limit");
-    button.removeAttribute("disabled");
-    button.style.backgroundColor = "blue";
-  }
+  playerList("messi", "select-messi");
 });
-
-/* ---------------------------------------------------------
------------------------------------------------------------- */
 document.getElementById("select-neymar").addEventListener("click", function () {
-  const playerId = document.getElementById("neymar");
-
-const button = document.getElementById("select-neymar");
-
-  const li = document.createElement("li");
-  li.innerText = playerId.innerText;
-  const ol = document.getElementById("player-list");
-
-  const playerList = document.querySelector("#player-list").childNodes.length;
-  if (playerList < 5) {
-    ol.appendChild(li);
-    button.disabled = true;
-    button.style.backgroundColor = "gray";
-  } else {
-    alert("There is no limit");
-    button.removeAttribute("disabled");
-    button.style.backgroundColor = "blue";
-  }
+  playerList("neymar", "select-neymar");
 });
-/* ---------------------------------------------------------
------------------------------------------------------------- */
 document.getElementById("select-mbappe").addEventListener("click", function () {
-  const playerId = document.getElementById("mbappe");
-
-const button = document.getElementById("select-mbappe");
-
-  const li = document.createElement("li");
-  li.innerText = playerId.innerText;
-  const ol = document.getElementById("player-list");
-
-  const playerList = document.querySelector("#player-list").childNodes.length;
-  if (playerList < 5) {
-    ol.appendChild(li);
-    button.disabled = true;
-    button.style.backgroundColor = "gray";
-  } else {
-    alert("There is no limit");
-    button.removeAttribute("disabled");
-    button.style.backgroundColor = "blue";
-  }
+  playerList("mbappe", "select-mbappe");
 });
-/* -----------------------------------------------------------
--------------------------------------------------------------- */
 document.getElementById("select-machado").addEventListener("click", function () {
-  const playerId = document.getElementById("machado");
-
-const button = document.getElementById("select-machado");
-
-  const li = document.createElement("li");
-  li.innerText = playerId.innerText;
-  const ol = document.getElementById("player-list");
-
-  const playerList = document.querySelector("#player-list").childNodes.length;
-  if (playerList < 5) {
-    ol.appendChild(li);
-    button.disabled = true;
-    button.style.backgroundColor = "gray";
-  } else {
-    alert("There is no limit");
-    button.removeAttribute("disabled");
-    button.style.backgroundColor = "blue";
-  }
+  playerList("machado", "select-machado");
 });
-/* -------------------------------------------------------------
----------------------------------------------------------------- */
 document.getElementById("select-ramos").addEventListener("click", function () {
-  const playerId = document.getElementById("ramos");
-
-
-const button = document.getElementById("select-ramos");
-
-  const li = document.createElement("li");
-  li.innerText = playerId.innerText;
-  const ol = document.getElementById("player-list");
-
-  const playerList = document.querySelector("#player-list").childNodes.length;
-  if (playerList < 5) {
-    ol.appendChild(li);
-    button.disabled = true;
-    button.style.backgroundColor = "gray";
-  } else {
-    alert("There is no limit");
-    button.removeAttribute("disabled");
-    button.style.backgroundColor = "blue";
-  }
+  playerList("ramos", "select-ramos");
 });
-/* ---------------------------------------------------------
------------------------------------------------------------- */
 document.getElementById("select-sanches").addEventListener("click", function () {
-  const playerId = document.getElementById("sanches");
-
-const button = document.getElementById("select-sanches");
-
-  const li = document.createElement("li");
-  li.innerText = playerId.innerText;
-  const ol = document.getElementById("player-list");
-
-  const playerList = document.querySelector("#player-list").childNodes.length;
-  if (playerList < 5) {
-    ol.appendChild(li);
-    button.disabled = true;
-    button.style.backgroundColor = "gray";
-  } else {
-    alert("There is no limit");
-    button.removeAttribute("disabled");
-    button.style.backgroundColor = "blue";
-  }
+  playerList("sanches", "select-sanches");
 });
-/*
----------------------------------------------------
----------------------------------------------------
- */
---
+++ JavaScript/utilities.js
@@ -0,0 +1,19 @@
+// <---------------Player List Common Function---------------> //
+
+function playerList(playerName, selectButton) {
+  const addedPlayer = document.getElementById(playerName);
+  const button = document.getElementById(selectButton);
+
+  const li = document.createElement("li");
+  li.innerText = addedPlayer.innerText;
+  const ol = document.getElementById("player-list");
+  const playerList = document.querySelector("#player-list").childNodes.length;
+
+  if (playerList < 5) {
+    ol.appendChild(li);
+    button.setAttribute("disabled", true);
+    button.style.backgroundColor = "gray";
+  } else {
+    alert("There is no limit");
+  }
+}
